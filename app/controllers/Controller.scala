eba55902d606af8bbf29510d4469d26c1044ff1e
LukeCaruzo <schmidt.lucas@googlemail.com>
2020-06-30 10:02:21 +0200

    fix

--- app/controllers/Controller.scala
+++ app/controllers/Controller.scala
@@ -8,5 +8,4 @@ import play.api.libs.json._
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class Controller @Inject()(val controllerComponents: ControllerComponents)(using ec: ExecutionContext) extends BaseController:
-  type Result = ErrorModel | List[Product]
+class Controller @Inject()(val controllerComponents: ControllerComponents)(using ec: ExecutionContext) extends BaseController
