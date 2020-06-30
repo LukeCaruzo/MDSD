d6ffa57aac9bea539aee50574cd0253e212acd88
LukeCaruzo <schmidt.lucas@googlemail.com>
2020-06-30 11:50:10 +0200

    works

--- app/controllers/Controller.scala
+++ app/controllers/Controller.scala
@@ -7,7 +7,6 @@ import play.api.mvc._
 import play.api.libs.json._
 import scala.concurrent.{ExecutionContext, Future}
 import service._
-import service.Error
 
 @Singleton
 class Controller @Inject()(val controllerComponents: ControllerComponents)(using ec: ExecutionContext) extends BaseController:
@@ -19,8 +18,5 @@ class Controller @Inject()(val controllerComponents: ControllerComponents)(using
 
   def index(): Action[String] = 
     Action.async(parse.text) { 
-          request => { request.body.parse
-          Future.successful(Ok(views.html.catalogue(item))) 
-          }
-          //case Right(error) => Future.successful(BadRequest(views.html.error()))
+          request => Future.successful(Ok(views.html.catalogue(request.body.parse)))
     }
--- app/model/Product.scala
+++ app/model/Product.scala
@@ -1,6 +1,5 @@
 package model
 
 import model.Item
-import model.Price
 
 case class Product(id: Int, name: String, image: String, price: Int, tags: String, description: String) extends Item(id, name)
