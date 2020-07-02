9e243f9ca89989031b2560a5db76f61429f93269
LukeCaruzo <schmidt.lucas@googlemail.com>
2020-07-02 12:59:42 +0200

    test

--- test/controllers/ControllerTest.scala
+++ test/controllers/ControllerTest.scala
@@ -9,8 +9,9 @@ import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{ RequestHeader, Result }
 import org.junit.Test
 import org.junit.Assert._
-
+import scala.language.implicitConversions
 import scala.concurrent.Future
+
 /**
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
@@ -18,18 +19,19 @@ import scala.concurrent.Future
  * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
  */
 class ControllerTest extends PlaySpec with GuiceOneAppPerTest {
-  val example = """
-  |catalogue 0 Bekleidung 
-  |product 0 T-Shirt /path/to/image 99 checked tshirt
-  |product 0 Hemd /path/to/image 99 striped hemd
-  |""".stripMargin
+  val example =
+    """
+      |catalogue 0 Bekleidung
+      |product 0 T-Shirt /path/to/image 99 checked tshirt
+      |product 0 Hemd /path/to/image 99 striped hemd
+      |""".stripMargin
 
-  "post" should {
-    "return answer" in {
-      val request = FakeRequest(POST, "/").withHeaders(HOST -> "localhost:9000").withBody(example)
-      val result = route(app, request).get
+  @Test def test(): Unit = {
+    // given ec as scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
+    val controller = new Controller(stubControllerComponents())
+    val request = FakeRequest(POST, "/").withHeaders(HOST -> "localhost:9000").withBody(example)
+    val home:Future[Result] = route(app, request).get
 
-      println(result)
-    }
+    assertEquals(status(home),OK)
   }
 }
